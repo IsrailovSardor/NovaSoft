04fe74bfa28f9f220f33da07a2a74978242add54
Estebes <estebesm95@gmail.com>
2022-08-11 18:46:46 +0600

    feature/added_parallax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.scss';
 import { Routes, Route} from "react-router-dom";
 import Main from "./pages/main/Main";
+import {ParallaxProvider} from "react-scroll-parallax";
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path={'/'} element={<Main/>}/>
-        </Routes>
+        <ParallaxProvider>
+            <Routes>
+              <Route path={'/'} element={<Main/>}/>
+            </Routes>
+        </ParallaxProvider>
     </div>
   );
 }
--- src/components/roadmap/Roadmap.jsx
+++ src/components/roadmap/Roadmap.jsx
@@ -2,6 +2,7 @@ import React, {useId} from 'react';
 import './roadmap.scss'
 import RoadmapItem from "./roadmapItem/RoadmapItem";
 import Divider from "./divider/Divider";
+import {Parallax} from "react-scroll-parallax";
 
 const items = [
     {
@@ -29,30 +30,32 @@ const items = [
 const Roadmap = () => {
     const id = useId()
     return (
-        <section className={'roadmap'}>
-            <div className="container">
-                <h1>
-                    Roadmap
-                </h1>
-                {
-                    items.map((item, index) =>
-                        <div key={`${id}__${item.title}__${index}`}
-                             className={'roadmap__item'}
-                        >
-                            <RoadmapItem title={item.title}
-                                         description={item.description}
-                                         image={require(`../../images/roadmap/${item.image}`)}
-                                         index={index+1}
-                            />
-                            {
-                                index+1 !== items.length &&
-                                <Divider/>
-                            }
-                        </div>
-                    )
-                }
-            </div>
-        </section>
+        <Parallax >
+            <section className={'roadmap'}>
+                <div className="container">
+                    <h1>
+                        Roadmap
+                    </h1>
+                    {
+                        items.map((item, index) =>
+                            <div key={`${id}__${item.title}__${index}`}
+                                 className={'roadmap__item'}
+                            >
+                                <RoadmapItem title={item.title}
+                                             description={item.description}
+                                             image={require(`../../images/roadmap/${item.image}`)}
+                                             index={index+1}
+                                />
+                                {
+                                    index+1 !== items.length &&
+                                    <Divider/>
+                                }
+                            </div>
+                        )
+                    }
+                </div>
+            </section>
+        </Parallax>
     );
 };
 

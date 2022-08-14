7e7db3c9c5ca024f2ca94cdf4f5855e94f32ca34
Estebes <estebesm95@gmail.com>
2022-08-14 14:40:42 +0600

    feature/added_property_to_fade/fade_works_once

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './aboutUs.scss'
+import FadeInSection from "../../hoc/fade/Fade";
 
 const AboutUs = () => {
     return (
         <section className={'aboutUs'}>
             <div className="container">
-                <h1>
-                    NOVASOFT is -
-                </h1>
-                <p>
-                    You do not choose your reputation.
-                    To be loved and trusted is not easy, it takes years.
-                    Building a brand isdeveloping a relationshipwith your customer.
-                    That’s where we step in. We uncover your essence. We turn skeptics into customers,
-                    customers into loyal fans.
-                </p>
-                <p>
-                    We are a rapidly growing group of open-minded people,
-                    sometimes a little crazy, perfectionists, who love white space and observe every detail.
-                    We are designers, developers, product managersbut above all people who love what
-                    they do and always give their best.
-                </p>
+                <FadeInSection>
+                    <h1>
+                        NOVASOFT is -
+                    </h1>
+                </FadeInSection>
+                <FadeInSection>
+                    <p>
+                        You do not choose your reputation.
+                        To be loved and trusted is not easy, it takes years.
+                        Building a brand isdeveloping a relationshipwith your customer.
+                        That’s where we step in. We uncover your essence. We turn skeptics into customers,
+                        customers into loyal fans.
+                    </p>
+                </FadeInSection>
+                <FadeInSection>
+                    <p>
+                        We are a rapidly growing group of open-minded people,
+                        sometimes a little crazy, perfectionists, who love white space and observe every detail.
+                        We are designers, developers, product managersbut above all people who love what
+                        they do and always give their best.
+                    </p>
+                </FadeInSection>
             </div>
         </section>
     );
